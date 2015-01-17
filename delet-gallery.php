8bf6826de882a6d249f2923dfa871dbbc0400029
sofyan <sofyanvian95@yahoo.com>
2015-01-17 07:01:35 +0700

    edit form upload banyak

--- delet-gallery.php
+++ delet-gallery.php
@@ -4,7 +4,7 @@ include "koneksi.php";
 	$sql    =   "delete from foto where id='$id'";
 	$query  =   mysql_query($sql);
 	if($query){
-	    echo "Hapus data berhasil";
+	    echo "<meta http-equiv=\"Refresh\" content=\"1;url=gallery.php\">";
 	}else{
 	    echo "Hapus data gagal";
 	}
--- formbanyak.php
+++ formbanyak.php
@@ -11,13 +11,13 @@ include "header.php"; ?>
 </div>
 <div id="pss">
 <div id="form-upload">
-<form action="save-gallery.php" method="post" enctype="multipart/form-data">
+<form action="save-banyak.php" method="post" enctype="multipart/form-data">
   <h3 align="center">Isikan judul dan pilih file yang akan di upload :</h3>
   <p> File  
 <?php $a= mysql_query("select * from foto order By id desc");
                 $row=mysql_fetch_array($a);
                 ?> <?php for($i=1;$i<=3;$i++){ ?>
-<input type="hidden" name="id<?php echo $row['id']+$i; ?>" value="<?php echo $row['id']+$i ;?>"/><br />Judul : <input name="deskripsi" type="text" size="30" maxlength="30" />  <input type="file" name="file" id="file" /> </p> <?php   echo 'id '.$row['id']+$i; } ?>
+<input type="hidden" name="id<?php echo $row['id']+$i; ?>" value="<?php echo $row['id']+$i ;?>"/><br />Judul : <input name="deskripsi<?php echo $i; ?>" type="text" size="30" maxlength="30" />  <input type="file" name="file<?php echo $i; ?>" id="file" /> </p> <?php } ?>
   <p>* Hanya file gambar jpg/gif/png</p>
 <p align="center"><input type="submit" name="save" value="Upload" /></p>
 </form>
